e0820b18c3864ecac486d34e620e8579ae969457
Sergei Smirnov <ss@ss.org>
2021-01-14 17:00:14 +0800

    added custom domain

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Spanner Protocol', // Title for your website.
   tagline: 'The blockchain business application layer',
-  url: 'https://spannerprotocol.github.io', // Your website URL
+  url: 'http://www.spanner.network', // Your website URL
   baseUrl: '/web-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
